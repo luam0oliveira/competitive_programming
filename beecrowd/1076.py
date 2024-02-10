fd907e41e27efbf325b7659967e1897fe6d8e6d0
Luam Alves de Oliveira <luamoliveira.d@gmail.com>
2024-02-10 12:47:20 -0300

    Refazendo o código 1076 pela forma mais 'correta'

--- beecrowd/1076.py
+++ beecrowd/1076.py
@@ -1,4 +1,46 @@
+import sys
+sys.setrecursionlimit(100000)
+
+# Most correct form
+def dfs(node, caminhos, visitados):
+    if visitados[node]:
+        return 0
+    elif not caminhos[node]:
+        visitados[node] = True
+        return 2
+    
+    i = 2
+
+    visitados[node] = True
+    while caminhos[node]:
+        paths = dfs(caminhos[node][0], caminhos, visitados)
+        i += paths
+        caminhos[node].pop(0)
+    return i
+
 def main():
+    cases = int(input())
+    for case in range(cases):
+        inicio = int(input())
+
+        num_nodes, num_caminhos = list(map(int, input().split()))
+
+        visitados = [False] * num_nodes
+        caminhos = []
+
+        for i in range(num_nodes):
+            caminhos.append([])
+
+        for i in range(num_caminhos):
+            orig, dest = list(map(int, input().split()))
+            caminhos[orig].append(dest)
+            caminhos[dest].append(orig)
+
+        print(dfs(inicio, caminhos, visitados) - 2)
+
+
+# Alternative form, the problem don't force to use graphs to resolve'
+def main1():
     cases = int(input())
     for case in range(cases):
         inicio = int(input())
