b6908590a6ca59d39376c668e1bc41841aa0f74e
Luam Alves de Oliveira <luamoliveira.d@gmail.com>
2024-08-23 11:07:57 -0300

    Correção H 22-08-24

--- topicos-especiais/lista-04/H.cpp
+++ topicos-especiais/lista-04/H.cpp
@@ -7,15 +7,15 @@ using namespace std;
 
 int matriz[200][200];
 string rs, mm;
+int aler;
 
 // -1: tipo 0; 1: tipo 1; 0: divide 
 int verificaTipo(int xi, int xf, int yi, int yf){
     // 0: zero, 1: um
     int cond[2] = {1,1};
-    for(int i = xi; i<xf;i++){
+    for(int i = xi; i<=xf;i++){
         if (!cond[0] && !cond[1]) break; 
-        for(int j=yi;j<yf;j++){
-            // cout<<i <<" "<<j<<" "<<matriz[i][j]<<endl;
+        for(int j=yi;j<=yf;j++){
             if (matriz[i][j]) cond[0] = 0;
             else cond[1] = 0;
         }
@@ -26,112 +26,94 @@ int verificaTipo(int xi, int xf, int yi, int yf){
     return 0;
 }
 
-// Corrigir
 void divide(int xi, int xf, int yi, int yf){
-    int variacaoX = xf-xi;
-    int variacaoY = yf-yi;
-
+    if (xi > xf || yi > yf) return;
     int tipo = verificaTipo(xi, xf, yi, yf);
+    if (tipo!=0){
+        rs += tipo == 1 ? '1':'0';
+        return;
+    }
+    rs+='D';
+    if (xf-xi+1 <= 2 && yf-yi + 1 <= 2){
+        for(int i = xi; i<=xf;i++)
+            for(int j = yi;j<=yf;j++)
+                rs+= to_string(matriz[i][j]);
+        return;
+    }
+    int xFim = (xf-xi)/2;
+    int yFim = (yf-yi)/2;
+    divide(xi, xi + xFim, yi, yi + yFim);
+    divide(xi, xi + xFim, yi + yFim + 1, yf);
+    divide(xi + xFim+1, xf, yi, yi + yFim);
+    divide(xi + xFim+1, xf, yi + yFim + 1, yf);
+}
 
-    if (variacaoX <= 2 && variacaoY <= 2){
-        if (!tipo){
-            rs+='D';
-            for(int i = xi;i<xf;i++){
-                for(int j=yi;j<yf;j++){
-                    rs+=to_string(matriz[i][j]);
-                }
-            }
-
-        }
-        else {
-            rs += tipo == 1 ? '1':'0';
+void unify(int xi, int xf, int yi, int yf){
+    if (xi > xf || yi > yf) return;
+    if(mm[aler] == 'D'){
+        aler++;
+        int xFim = (xf-xi)/2;
+        int yFim = (yf-yi)/2;
+        if (xf-xi+1 <= 2 && yf-yi + 1 <= 2){
+            for(int i=xi;i<=xf;i++)
+                for(int j=yi;j<=yf;j++)
+                    unify(i, i,j, j);
+        }else{
+            unify(xi, xi + xFim, yi, yi + yFim);
+            unify(xi, xi + xFim, yi + yFim + 1, yf);
+            unify(xi+xFim+1, xf, yi, yi + yFim);
+            unify(xi+xFim+1, xf, yi + yFim + 1, yf);
         }
         return;
     }
-
-    if (!tipo){
-        rs+='D';
-        int metadeX = xi+(variacaoX+1)/2;
-        int metadeY = yi+(variacaoY+1)/2;
-        // Esquerda Superior
-        divide(xi,metadeX, yi, metadeY);
-        // Direita Superior
-        divide(xi,metadeX, metadeY, yf);
-        // Esquerda Inferior
-        divide(metadeX,xf, yi, metadeY);
-        // Direita Inferior
-        divide(metadeX,xf, metadeY, yf);
-    }else if (tipo == 1)
-        rs+='1';
-    else
-        rs+='0';
+    for(int i=xi;i<=xf;i++)
+        for(int j=yi;j<=yf;j++)
+            matriz[i][j] = mm[aler]=='1';
+        
+    aler++;
 }
 
-// void codifica(int pos,int xi, int xf, int yi, int yf){
-//     int variacaoX = xf-xi;
-//     int variacaoY = yf-yi;
-
-//     if (mm[pos] == 'D'){
-
-//     }
-//     if (variacaoX <= 2 && variacaoY <= 2){
-//         if (!tipo){
-//             rs+='D';
-//             for(int i = xi;i<xf;i++){
-//                 for(int j=yi;j<yf;j++){
-//                     rs+=to_string(matriz[i][j]);
-//                 }
-//             }
-
-//         }
-//         else {
-//             rs += tipo == 1 ? '1':'0';
-//         }
-//         return;
-//     }
-
-//     if (!tipo){
-//         rs+='D';
-//         int metadeX = xi+(variacaoX+1)/2;
-//         int metadeY = yi+(variacaoY+1)/2;
-//         // Esquerda Superior
-//         divide(xi,metadeX, yi, metadeY);
-//         // Direita Superior
-//         divide(xi,metadeX, metadeY, yf);
-//         // Esquerda Inferior
-//         divide(metadeX,xf, yi, metadeY);
-//         // Direita Inferior
-//         divide(metadeX,xf, metadeY, yf);
-//     }else if (tipo == 1)
-//         rs+='1';
-//     else
-//         rs+='0';
-// }
-
 int main(){
-    char op;
-    while(cin>>op && op!='#'){
+    string op;
+    while(cin>>op && op!="#"){
         rs = "";
+        aler=0;
         int n,m;cin>>n>>m;
-        cin>>mm;
 
-    
-        if (op == 'B'){
+        if (op == "B"){
+            string ff;
+            int cont=0;
+            while(n*m != cont){
+                cin>>mm;
+                ff+=mm;
+                cont+=mm.size();
+            }
+
             for(int i=0;i<n;i++)
                 for(int j=0;j<m;j++)
-                    matriz[i][j] = mm[m*i + j] == '1';
-            divide(0,n,0,m);
-            int cont = 0;
+                    matriz[i][j] = ff[m*i + j] == '1';
+            divide(0,n-1,0,m-1);
+            cont = 0;
+            printf("D%4d%4d\n", n,m);
             for(int i = 0;i<rs.size();i++){
                 cout<<rs[i];
                 cont++;
                 if (cont%50==0) cout<<endl;
             }
-            cout<<endl;
+            if (cont%50) cout<<endl;
         }else{
-
+            cin>>mm;
+            unify( 0, n-1, 0, m-1);
+            int cont=0;
+            printf("B%4d%4d\n", n,m);
+            for(int i = 0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    cout<<matriz[i][j];
+                    cont++;
+                    if (cont%50==0) cout<<endl;
+                }
+            }
+            if (cont%50) cout<<endl;
         }
     }
-
-
 }
